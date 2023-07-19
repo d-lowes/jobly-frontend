eca4b0759ef72d0471b0e8ca4ce36343562f9a43
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-07-18 20:54:41 -0700

    style navbar and add logo image

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,5 +1,4 @@
-import React from "react";
-import { NavLink } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
 import { useContext } from "react";
 import userContext from "./userContext";
 
@@ -15,44 +14,68 @@ function NavBar({ logout }) {
   const { user } = useContext(userContext);
 
   /** Show login and signup on the NavBar if no user is logged in */
+
   function loggedOutNav() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink className="nav-item nav-link" to="/">Jobly</NavLink>
-        <div className="navbar" id="navbarNav">
-          <NavLink className="nav-item nav-link" to="/login">Login</NavLink>
-          <NavLink className="nav-item nav-link" to="/signup">Signup</NavLink>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark" data-bs-theme="dark">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">Jobly</NavLink>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/login">Login</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/signup">Signup</NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
   }
-
   /** Show Jobs, Companies, logout, and Profile on the NavBar ifuser is logged
-   *  in
-  */
+    *  in
+    */
   function loggedInNav() {
     return (
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <NavLink className="nav-item nav-link" to="/">Jobly</NavLink>
-        <div className="navbar" id="navbarNav">
-          <NavLink className="nav-item nav-link" to="/companies">
-            Companies
-          </NavLink>
-          <NavLink className="nav-item nav-link" to="/jobs">Jobs</NavLink>
-          <NavLink className="nav-item nav-link" to="/profile">Profile</NavLink>
-          <NavLink className="nav-item nav-link" to="/" onClick={logout}>
-            Logout {user.username}
-          </NavLink>
+      <nav className="navbar navbar-expand-lg navbar-light bg-dark" data-bs-theme="dark">
+        <div className="container-fluid">
+          <NavLink className="navbar-brand" to="/">Jobly</NavLink>
+          <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
+          </button>
+          <div className="collapse navbar-collapse" id="navbarColor01">
+            <ul className="navbar-nav me-auto">
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/companies">Companies</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/jobs">Jobs</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/profile">Profile</NavLink>
+              </li>
+              <li className="nav-item">
+                <NavLink className="nav-link" to="/" onClick={logout}>
+                  Logout {user.username}
+                </NavLink>
+              </li>
+            </ul>
+          </div>
         </div>
       </nav>
     );
   }
 
-  if (!user) {
-    return loggedOutNav();
-  } else {
-    return loggedInNav();
-  }
+  return (
+    <nav>
+      {user ? loggedInNav() : loggedOutNav()}
+    </nav>
+  );
 }
 
 export default NavBar;
