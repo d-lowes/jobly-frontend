40349610e54fb18b02e19b0e68ffebc33a13e0ed
Dylan Lowes <121920809+d-lowes@users.noreply.github.com>
2023-07-25 21:49:53 -0700

    ui improvements
    center profile, login, and signup forms with appropriate spacing and styling
    center homepage title section and size elements properly
    add customization to color scheme

--- src/Forms/SignupForm.js
+++ src/Forms/SignupForm.js
@@ -41,71 +41,80 @@ function SignupForm({ signup }) {
   }
 
   return (
-    <div className="registration-form">
-      <form onSubmit={handleSubmit}>
+    <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+      <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+        <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+          <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+            Create an account
+          </h1>
+          <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="username-input">Username:</label>
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 form-control item"
+                name="username"
+                id="username-input"
+                value={formData.username}
+                onChange={handleChange}
+              ></input>
+            </div>
 
-        <div className="form-group">
-          <label htmlFor="username-input">Username:</label>
-          <input
-            className="form-control item"
-            name="username"
-            id="username-input"
-            value={formData.username}
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="password-input">Password:</label>
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 form-control item"
+                name="password"
+                id="password-input"
+                value={formData.password}
+                type="password"
+                onChange={handleChange}
+              ></input>
+            </div>
 
-            onChange={handleChange}
-          ></input>
-        </div>
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="firstName-input">First Name:</label>
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 form-control item"
+                name="firstName"
+                id="firstName-input"
+                value={formData.firstName}
+                onChange={handleChange}
+              ></input>
+            </div>
 
-        <div className="form-group">
-          <label htmlFor="password-input">Password:</label>
-          <input
-            className="form-control item"
-            name="password"
-            id="password-input"
-            value={formData.password}
-            type="password"
-            onChange={handleChange}
-          ></input>
-        </div>
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="lastName-input">Last Name:</label>
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 form-control item"
+                name="lastName"
+                id="lastName-input"
+                value={formData.lastName}
+                onChange={handleChange}
+              ></input>
+            </div>
 
-        <div className="form-group">
-          <label htmlFor="firstName-input">First Name:</label>
-          <input
-            className="form-control item"
-            name="firstName"
-            id="firstName-input"
-            value={formData.firstName}
-            onChange={handleChange}
-          ></input>
-        </div>
+            <div>
+              <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white" htmlFor="email-input">Email:</label>
+              <input
+                className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 form-control item"
+                name="email"
+                id="email-input"
+                value={formData.email}
+                onChange={handleChange}
+              ></input>
+            </div>
 
-        <div className="form-group">
-          <label htmlFor="lastName-input ">Last Name:</label>
-          <input
-            className="form-control item"
-            name="lastName"
-            id="lastName-input"
-            value={formData.lastName}
-            onChange={handleChange}
-          ></input>
+            <div>
+              <button className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800 btn-primary btn-block create-account">
+                Submit
+              </button>
+            </div>
+          </form>
         </div>
-
-        <div className="form-group">
-          <label htmlFor="email-input">Email:</label>
-          <input
-            className="form-control item"
-            name="email"
-            id="email-input"
-            value={formData.email}
-            onChange={handleChange}
-          ></input>
-        </div>
-        <div>
-          <button className="btn-primary btn-block create-account">Submit</button>
-        </div>
-      </form>
+      </div>
     </div>
   );
+
 
 }
 
--- src/Home.js
+++ src/Home.js
@@ -9,10 +9,12 @@ function Home() {
   const { user } = useContext(userContext);
 
   return (
-    <div className="bg-job-hiring bg-contain h-screen flex flex-col items-center">
-      <h1 className="text-white">Jobly</h1>
-      <h3  className="text-white">All the jobs in one, convenient place.</h3>
-      {user && <h4 className="text-white"> Hello, {user.username}!</h4>}
+    <div className="bg-job-hiring bg-contain h-screen flex flex-col items-center justify-center">
+      <div className="bg-black flex flex-col items-center justify-center px-6 py-8 mx-auto lg:py-4">
+        <h1 className="text-white mb-4 font-extrabold leading-none tracking-tight text-gray-900 text-5xl">Jobly</h1>
+        <h3 className="text-white text-2xl">All the jobs in one, convenient place.</h3>
+        {user && <h4 className="text-white text-1xl pt-2"> Hello, {user.username}!</h4>}
+      </div>
     </div>
   );
 }
